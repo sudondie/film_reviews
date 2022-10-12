a2e295a51bf99cb9031d97672254d8c144a38922
Ilia Skakov <ilia.skakov@gmail.com>
2022-10-12 16:29:30 +0300

    Залил проект

---
+++ src/components/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HeaderView/>
+    <div v-for="review in reviews" :key="review.id">
+      {{review.filmName}} <br>
+      {{review.grade}}
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderView from './HeaderView.vue';
+export default {
+  data() {
+    return {
+      reviews: [],
+    }
+  },
+  components: {
+    HeaderView,
+  },
+  mounted() {
+    this.reviews = JSON.parse(localStorage.getItem('reviews'));
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '../components/HomeView.vue';
+import NewFilm from '../components/NewFilm.vue';
 
 Vue.use(VueRouter);
 
@@ -11,15 +12,11 @@ const routes = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/newFilm',
+    name: 'newFilm',
+    component: NewFilm,
   },
 ];
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
